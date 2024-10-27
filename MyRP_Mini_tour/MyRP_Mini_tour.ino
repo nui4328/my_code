5ddc6b9d934a3fdb9ed138ad75fab42df688418f
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2024-10-27 08:51:35 +0700

    Add files via upload

---
+++ MyRP_Mini_tour/MyRP_Mini_tour.ino
@@ -0,0 +1,59 @@
+#include <my_MINI_RP.h>
+#define EEPROM_ADDR 0x50
+#include <EncoderLibrary.h>
+EncoderLibrary encoder(6, 7, 15, 20);
+#include <my_TCS34725.h> 
+
+float rotA, rot, high, lowA, low;
+float theta1,theta2,theta3;
+#define ROBOT_ARM_LOW 80
+#define ROBOT_ARM_HIGH 75
+#define ROBOT_ARM_EE_OFFSET 0
+
+int servo_tim8 = 0;
+int servo_tim0 = 0;
+int servo_tim1 = 0;
+// Number of steps
+int num_steps = 20;
+
+// Initial angles
+float s8_before_deg = 90 + servo_tim8;
+float s0_before_deg = 90 + servo_tim0;
+float s1_before_deg = 90 ;
+
+
+void setup() 
+  {    
+      _seting();                 //-------------------->> ฟังก์ชัน ตั้งค่าต่าง ๆ ใน tab seting
+      encoder.setupEncoder();    //-------------------->> เรียกฟังก์ชัน setupEncoder
+      encoder.resetEncoders();  //--------------------->> ฟังก์ชันรอก
+     
+
+      //arm_down_open();
+      //servo(8, 90);
+      
+      sw();             //-------------------->> ฟังก์ชันรอกดปุ่ม 
+      //test_bord(); sw();      
+  
+///------------------------------------------------------------------>>>>  เขียนโค๊ดที่นี่      
+   /*
+   co_xyz(75, 80, 40); 
+   co_xyz(40, -40, 40); delay(1000);
+   move_bed(180); delay(1000);
+   move_bed(90); delay(1000);
+   */
+   //start_game();
+   tour_1();
+
+///------------------------------------------------------------------>>>>  เขียนโค๊ดที่นี่ 
+
+  }  ///-->> บรรทัดห้ามลบ
+
+
+///-------------------------->> คงไว้ห้ามลบ
+void loop() {
+
+     Serial.println(encoder.Poss_R());
+     delay(10);
+  }
+///-------------------------->> คงไว้ห้ามลบ
---
+++ MyRP_Mini_tour/funtion_tour.ino
@@ -0,0 +1,115 @@
+void T_right(int sp, int deg)
+  {
+    int en_coder = map(deg, 0, 90, 0, 300);
+    encoder.resetEncoders();   //----->> encoder = 0
+    do{ Motor(sp, -sp); }while( encoder.Poss_L() < en_coder );
+    Motor(-sp, sp); delay(40);
+    Motor(-1, 1); delay(40);
+    if(mcp_f(3) > md_mcp_f(3) && mcp_f(2) < md_mcp_f(2))
+      {
+        do{Motor(-30, 30);}while(mcp_f(3) > md_mcp_f(3));
+        Motor(30, -30); delay(20);
+        Motor(0, 0); delay(20);
+      }
+    else if(mcp_f(4) > md_mcp_f(4) && mcp_f(5) < md_mcp_f(5))
+      {
+        do{Motor(30, -30);}while(mcp_f(4) > md_mcp_f(4));
+        Motor(-30, 30); delay(20);
+        Motor(0, 0); delay(20);
+      }
+  }
+
+void T_left(int sp, int deg)
+  {
+    int en_coder = map(deg, 0, 90, 0, 300);
+    encoder.resetEncoders();   //----->> encoder = 0
+    do{ Motor(-sp, sp); }while( encoder.Poss_R() < en_coder );
+    Motor(sp, -sp); delay(40);
+    Motor(1, -1); delay(40);
+    if(mcp_f(3) > md_mcp_f(3) && mcp_f(2) < md_mcp_f(2))
+      {
+        do{Motor(-30, 30);}while(mcp_f(3) > md_mcp_f(3));
+        Motor(30, -30); delay(20);
+        Motor(0, 0); delay(20);
+      }
+    else if(mcp_f(4) > md_mcp_f(4) && mcp_f(5) < md_mcp_f(5))
+      {
+        do{Motor(30, -30);}while(mcp_f(4) > md_mcp_f(4));
+        Motor(-30, 30); delay(20);
+        Motor(0, 0); delay(20);
+      }
+  }
+
+void T_right_sensor_F(int sp, int deg)
+  {
+    
+    encoder.resetEncoders();   //----->> encoder = 0
+    do{ Motor(sp, -sp); }while( encoder.Poss_L() < 150 );
+    do{ Motor(sp, -sp); }while( mcp_f(deg) > min_mcp_f(deg)+70);
+    Motor(-sp, sp); delay(30);
+    Motor(-1, 1); delay(40);
+    
+  }
+
+void T_right_sensor_B(int sp, int deg)
+  {
+    
+    encoder.resetEncoders();   //----->> encoder = 0
+    do{ Motor(sp, -sp); }while( encoder.Poss_L() < 150 );
+    do{ Motor(sp, -sp); }while( mcp_b(deg) > min_mcp_b(deg)+70);
+    Motor(-sp, sp); delay(30);
+    Motor(-1, 1); delay(40);
+    
+  }
+
+void T_left_sensor_F(int sp, int deg)
+  {
+    
+    encoder.resetEncoders();   //----->> encoder = 0
+    do{ Motor(-sp, sp); }while( encoder.Poss_L() > -150 );
+    do{ Motor(-sp, sp); }while( mcp_f(deg) > min_mcp_f(deg)+70);
+    Motor(sp, -sp); delay(30);
+    Motor(-1, 1); delay(40);
+    
+  }
+
+void T_left_sensor_B(int sp, int deg)
+  {
+    
+    encoder.resetEncoders();   //----->> encoder = 0
+    do{ Motor(-sp, sp); }while( encoder.Poss_L() > -150 );
+    do{ Motor(-sp, sp); }while( mcp_b(deg) > min_mcp_b(deg)+70);
+    Motor(sp, -sp); delay(30);
+    Motor(-1, 1); delay(40);
+    
+  }
+void move_F_center()
+  {
+    encoder.resetEncoders();
+    while(encoder.Poss_R() < 120)
+      {
+            float error_L = map(mcp_f(3), min_mcp_f(3), max_mcp_f(3), 0, 20 );
+            float error_R = map(mcp_f(4), min_mcp_f(4), max_mcp_f(4), 0, 20 );
+            errors = error_L - error_R;     
+            P = errors;
+            I = I + errors;
+            D = errors - previous_error;                    
+            previous_error=errors  ;
+            PID_output = (0.15 * P) + (0.00001 * I) + (0.15* D); 
+            
+            Motor(20 + PID_output, 20 - PID_output);delayMicroseconds(50);
+      }
+    Motor(-20, -20); delay(30);
+    Motor(-1, 1); delay(100);
+  }
+
+void move_B_center()
+  {
+    encoder.resetEncoders();
+    while(encoder.Poss_R() > -120)
+      {
+        Motor(-20, -20);  delayMicroseconds(50);  
+      }
+    Motor(20, 20); delay(30);
+    Motor(-1, -1); delay(100);
+  }
